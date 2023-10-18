b4bb6d5fe91403f26ade1471bcb8e3d839920152
Safayat-Hasan <safayathasananim20@gmail.com>
2023-10-18 22:07:01 +0600

    initiated the server on local host

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Brand-shop server running")
+})
+
+app.listen(port, () => {
+    console.log(`Brand-shop server running on port: ${port}`)
+})
